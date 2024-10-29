0eae76a2542e59bde6de6544deb619786db17d32
Sukumar Anup Sukumaran <sukumars@pyronix.com>
2024-10-29 11:13:40 +0000

    Initial Commit

---
+++ ZoomPanView/ZoomPanViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomPanViewApp.swift
+//  ZoomPanView
+//
+//  Created by Sukumar.Sukumaran on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ZoomPanViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
